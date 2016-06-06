127aeb343c43f0c296fc7ab7ca2c772524739ecd
Shinya Yanagihara <shinya.com@gmail.com>
2016-06-06 13:01:59 +0000

    Initial commit
    I add a recipe for deploying a oracle inventory file.

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: oracle-inventory
+# Recipe:: default
+#
+# Copyright (c) 2016 Shinya Yanagihara, All Rights Reserved.
+# log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting execution phase"
+puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting compile phase"
+
+
+# log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished execution phase"
+puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished compile phase"
