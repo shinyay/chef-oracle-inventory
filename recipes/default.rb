01b1dd874104fc1b38d7a844fc7ad4bcbf91272e
Shinya Yanagihara <shinya.com@gmail.com>
2016-06-08 10:58:12 +0000

    Add loggin of start and end time to the recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,9 @@
 # log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting execution phase"
 puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting compile phase"
 
+[:platform, :platform_version, :ipaddress, :fqdn, :hostname].each do |key|
+  puts "node['#{key}'] = #{node[key]}"
+end
 
 # log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished execution phase"
 puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished compile phase"
